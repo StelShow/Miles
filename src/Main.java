3249a6f4e746579d32dc6810562ccfe8f6cb195d
denmoz21@ya.ru <denmoz21@ya.ru>
2022-07-12 16:44:07 +0300

    reformate code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-   int coast = 100;
+        int coast = 100;
         int total = 20;
         int miles = coast / total;
-        System.out.println( "Вам начислят " + miles + " миль" );
+        System.out.println("Вам начислят " + miles + " миль");
     }
 }
